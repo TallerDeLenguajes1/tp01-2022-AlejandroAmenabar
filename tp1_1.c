b9be9ecbc4042e0862824dca5a30b2a641e8a253
Alejandro Amenabar <alegymnas@gmail.com>
2022-03-17 19:26:03 -0300

    6commit

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 
 int main(){
-
     printf("\n\tHola mundo!");
+    
+    int *punt;
+    int x=3;
+    punt=&x;
+    printf("\nEl contenido del puntero es %i",*punt);
+    printf("\nla direccion de memoria almacenada por el puntero es %i",punt);
+    printf("\nla direccion de memoria de la variable es: %i",&x);
+    printf("\nla direccion de memoria del puntero es: %i",&punt);
+    printf("\nEl tamano de memoria utilizado por x: %i",sizeof(int));
+
+    
     return 0;
 }
